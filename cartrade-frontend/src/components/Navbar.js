d52d4872d73c6a6cc9e8e8881603e21ba792301e
Shivani Sharma <92907798+05912Shivani@users.noreply.github.com>
2025-08-07 12:34:39 +0530

    Navbar.js

--- cartrade-frontend/src/components/Navbar.js
+++ cartrade-frontend/src/components/Navbar.js
@@ -12,7 +12,7 @@ const AppNavbar = () => {
 
   const handleLogout = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/auth/logout', {
+      const response = await fetch('https://cartrade-backend-9y1g.onrender.com', {
         method: 'POST',
         credentials: 'include',
       });
